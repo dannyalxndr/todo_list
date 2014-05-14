222f2145a6a697c1285d6a3f13977ea22afad5c5
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-14 14:24:17 -0500

    updated file to allow lowercase letters

--- todo.php
+++ todo.php
@@ -1,12 +1,24 @@
 <?php
 
+// Create a new directory in your vagrant-lamp directory named todo_list with a file named todo.php containing the code above. Use git init to initialize a new local repository in that directory and commit your code. Create a new remote repository on GitHub called CLI_Todo_List and add the remote to your local repository so you can push your code.
+
+// After each exercise item, commit and push changes to your GitHub repository.
+
+// Update the code to allow upper and lowercase inputs from user for all menu items. Test adding, removing, and quitting.
+
+// Update the program to start numbering the list with 1 instead of 0. Make sure remove still works as expected.
+
+
+
 // Create array to hold list of todo items
 $items = array();
 
 // The loop!
-do {
+do 
+{
     // Iterate through list items
-    foreach ($items as $key => $item) {
+    foreach ($items as $key => $item) 
+    {
         // Display each item and a newline
         echo "[{$key}] {$item}\n";
     }
@@ -19,12 +31,15 @@ do {
     $input = trim(fgets(STDIN));
 
     // Check for actionable input
-    if ($input == 'N') {
+    if (($input == 'N') || ($input == 'n')) 
+    {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
         $items[] = trim(fgets(STDIN));
-    } elseif ($input == 'R') {
+    } 
+    elseif (($input == 'R') || ($input == 'r'))
+    {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
@@ -33,10 +48,13 @@ do {
         unset($items[$key]);
     }
 // Exit when input is (Q)uit
-} while ($input != 'Q');
+} 
+while (($input != 'Q') || ($input !== 'q'));
 
 // Say Goodbye!
 echo "Goodbye!\n";
 
 // Exit with 0 errors
 exit(0);
+
+
