d5efd6f8fd70e5103b2e119de532df55f45c4050
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-21 09:00:00 -0500

    updated todo list with open file function

--- todo.php
+++ todo.php
@@ -1,47 +1,125 @@
 <?php
 
-// Create a new directory in your vagrant-lamp directory named todo_list with a file named todo.php containing the code above. Use git init to initialize a new local repository in that directory and commit your code. Create a new remote repository on GitHub called CLI_Todo_List and add the remote to your local repository so you can push your code.
-
-// After each exercise item, commit and push changes to your GitHub repository.
-
-// Update the code to allow upper and lowercase inputs from user for all menu items. Test adding, removing, and quitting.
-
-// Update the program to start numbering the list with 1 instead of 0. Make sure remove still works as expected.
-
-
-
-// Create array to hold list of todo items
 $items = array();
+
+
+// function to display the results of your items list
+function list_items($list)
+{
+    $result = '';
+
+    foreach ($list as $key => $value)
+    {
+        $result .= "[" . ($key + 1) . "] {$value}\n";  
+    }
+    return $result;
+}
+
+// function that reads the input of your user
+function get_input($upper = false) 
+{
+    $result = trim(fgets(STDIN));
+    return $upper ? strtoupper($result) : $result;
+}
+
+// function for sorting your items menu how you'd like
+function sort_menu($items)
+{
+    echo "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered: ";
+    $input = get_input(true); 
+
+    // sort the $items
+    switch ($input) 
+    {
+        case 'A':
+            asort($items, SORT_NATURAL|SORT_FLAG_CASE);
+            break;
+        case 'Z':
+            arsort($items, SORT_NATURAL|SORT_FLAG_CASE);
+            break;
+        case 'O':
+            ksort($items, SORT_NATURAL|SORT_FLAG_CASE);
+            break;
+        case 'R':
+            krsort($items, SORT_NATURAL|SORT_FLAG_CASE);
+            break;
+    }
+
+    return $items;   
+}
+
+// Create a function that reads the file, and adds each line to the current 
+// TODO list. Loading data/list.txt should properly load the list from above. 
+// Be sure to fclose() the file when you are done reading it.
+function read_file()
+{
+    echo "Enter the filename: ";
+    $filename = get_input();
+    $handle = fopen($filename, 'r');
+    $contents = trim(fread($handle, filesize($filename)));
+    return $contents;
+    fclose($handle);
+}
 
 do 
 {
-    foreach ($items as $key => $item) 
+    echo list_items($items);
+    echo '(N)ew item, (R)emove item, (S)ort, (O)pen file, (Q)uit : ';
+    $input = get_input(true);
+    
+    if ($input == 'N') 
     {   
-        $key++;
-        echo "[{$key}] {$item}\n";
-    }
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
-    
-    $input = trim(fgets(STDIN));
-    
-    if (($input == 'N') || ($input == 'n')) 
-    {
-        echo 'Enter item: ';
-        $items[] = trim(fgets(STDIN));
+        echo "Enter item: ";
+        $item = trim(fgets(STDIN));
+
+        echo "Add this item to the [B]eginning or [E]nd of list: ";
+        $b_and_e = get_input(true);
+
+        if ($b_and_e == 'B')
+        {
+            array_unshift($items, $item);
+        }
+        elseif ($b_and_e == 'E')
+        {
+            array_push($items, $item);
+        }
     } 
-    
-    elseif (($input == 'R') || ($input == 'r'))
+    elseif ($input == 'R') 
     {
         echo 'Enter item number to remove: ';
-        $key = trim(fgets(STDIN)) - 1;
+        $key = get_input();
+        $key--;
         unset($items[$key]);
         $items = array_values($items);
     }
+    elseif ($input == 'F')
+    {
+        array_shift($items);
+    }
+    elseif ($input == 'L')
+    {
+        array_pop($items);
+    }
+    elseif ($input == 'S')
+    {
+        $items = sort_menu($items);
+    }
+    elseif ($input == 'O')
+    {      
+        $contents = read_file();
+        // explode the string into an array
+        $content_array = explode("\n", $contents);
+        $items = array_merge($items, $content_array);
+    }
 } 
-while (($input != 'Q') && ($input != 'q'));
+while ($input != 'Q');
 
 echo "Goodbye!\n";
-
 exit(0);
+
+
+
+
+
 
 
